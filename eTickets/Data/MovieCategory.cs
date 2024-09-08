194a910f28897919fedf8d360281ee7226234bb4
Colin Sexton <colin.sexton.1207@gmail.com>
2024-09-07 19:47:15 -0500

    Added EntityFramework dependencies.

---
+++ eTickets/Data/MovieCategory.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data {
+    public enum MovieCategory {
+        Action,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace eTickets.Models {
-    public class Actor {
+    public class Producer {
         [Key]
         public int producerId { get; set; }
+        
         public string profilePictureUrl { get; set; }
         public string fullName { get; set; }
         public string bio { get; set; }
