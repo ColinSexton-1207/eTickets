ebcc8e13a03ce83bd68eeb0ae21f252b55d8c6b5
Colin Sexton <colin.sexton.1207@gmail.com>
2024-09-08 21:20:18 -0500

    Added Movies Controller

--- eTickets/obj/Debug/net8.0/eTickets.AssemblyInfo.cs
+++ eTickets/obj/Debug/net8.0/eTickets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bad6b7ff287b85f254dae4c9650ed2e5d7674b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7756df9b2e6e61f312f03ab4c4d20b2c7d5dce41")]
 [assembly: System.Reflection.AssemblyProductAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
