7756df9b2e6e61f312f03ab4c4d20b2c7d5dce41
Colin Sexton <colin.sexton.1207@gmail.com>
2024-09-08 21:17:51 -0500

    Added CinemasController. Updated ActorsController w/ async support

--- eTickets/obj/Debug/net8.0/eTickets.AssemblyInfo.cs
+++ eTickets/obj/Debug/net8.0/eTickets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60506b0ce7fe55554cc6c9be35d34b28b2b0d618")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bad6b7ff287b85f254dae4c9650ed2e5d7674b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
