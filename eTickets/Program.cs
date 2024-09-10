a7088ea6b9545988c9d37ffffd3a662afa861252
Colin Sexton <colin.sexton.1207@gmail.com>
2024-09-10 18:12:25 -0500

    Realized my mistake, forgot to add the service to Program.cs

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(options => {
     options.UseSqlServer(connectionString);
 });
+
+builder.Services.AddScoped<IActorsService, ActorsService>();
 
 var app = builder.Build();
 
--- eTickets/obj/Debug/net8.0/eTickets.AssemblyInfo.cs
+++ eTickets/obj/Debug/net8.0/eTickets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09767a2744ba16d33a493bbedb28a148ca36776d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8637a9e5b83682743ccdb67e5c06494af4d5f830")]
 [assembly: System.Reflection.AssemblyProductAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
